f4e386d9f63270f930f03184a83a5e74f1bff339
akash das <aku.das02@gmail.com>
2023-08-07 18:46:32 +0600

    style store page and cart

---
+++ src/components/store/StoreItem.tsx
@@ -0,0 +1,51 @@
+import { formatCurrency } from '../../utils/formatCurrency';
+
+type StoreItemProps = {
+  id: number;
+  name: string;
+  price: number;
+  imgUrl: string;
+};
+
+const StoreItem = ({ name, price, imgUrl }: StoreItemProps) => {
+  const quantity = 0;
+
+  return (
+    <div className='max-w-sm h-full rounded overflow-hidden shadow-lg'>
+      <img className='w-full h-[200px] object-cover' src={imgUrl} alt='Image' />
+      <div className='flex justify-between items-center px-4 py-4'>
+        <p className='font-bold text-xl text-gray-900'>{name}</p>
+        <span className='font-bold text-lg text-gray-700'>
+          {formatCurrency(price)}
+        </span>
+      </div>
+      <div className='px-4 pt-2 pb-2 mt-auto mb-2'>
+        {quantity === 0 ? (
+          <button className='bg-blue-600 hover:bg-blue-700 text-white w-full rounded p-1'>
+            + Add To Cart
+          </button>
+        ) : (
+          <div className='flex items-center flex-col gap-2'>
+            <div className='flex items-center justify-center gap-2'>
+              <button className='bg-blue-600 hover:bg-blue-700 text-white rounded flex justify-center items-center w-8 h-8 text-2xl'>
+                -
+              </button>
+              <div>
+                <span className='text-xl'>{quantity} </span>
+                in cart
+              </div>
+              <button className='bg-blue-600 hover:bg-blue-700 text-white rounded flex justify-center items-center w-8 h-8 text-2xl'>
+                +
+              </button>
+            </div>
+            <button className='bg-red-600 hover:bg-red-700 text-white rounded text-md px-2 py-1'>
+              Remove
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default StoreItem;
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,5 +1,18 @@
+import StoreItem from '../components/store/StoreItem';
+import storeItems from '../data/items.json';
+
 const Store = () => {
-  return <div>Store</div>;
+  return (
+    <>
+      <h1>Store</h1>
+
+      <div className='grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8'>
+        {storeItems.map((item) => (
+          <StoreItem key={item.id} {...item} />
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default Store;
